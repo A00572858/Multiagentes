8b66cd0131ec6ee3412f9b56a4609b2692109cc5
A00572858 <A00572858@itesm.mx>
2022-11-25 16:12:33 -0600

    api

--- reto.py
+++ reto.py
@@ -20,28 +20,6 @@ from threading import Timer
 import json
 
 # --------------------------------------------- #
-# -------------------- API -------------------- #
-# --------------------------------------------- #
-
-def UNITY_GET(model):
-
-    varsthingy = {}
-    for agent in model.schedule.agent_buffer(False):
-        if agent.pos != None:
-            lane = agent.pos[0]
-        else:
-            lane = -1
-        varsthingy[agent.unique_id] = {
-            "id" : agent.unique_id,
-            "speed" : agent.speed,
-            "lane" : int(lane)
-        }
-
-    jsonOut = json.dumps(varsthingy, sort_keys=True)
-
-    return jsonOut
-
-# --------------------------------------------- #
 # ------------------- AGENT ------------------- #
 # --------------------------------------------- #
 
@@ -179,22 +157,100 @@ last_step_time = 0
 fraction = 1 / STEPS_PER_SECOND
 
 model = Highway(totalSteps, (STEPS_PER_SECOND * TIME_STOP), STEPS_PER_SECOND)
-for i in range(totalSteps):
-    timer = Timer(fraction, model.step())
-    print(UNITY_GET(model))
+# for i in range(totalSteps):
+#     timer = Timer(fraction, model.step())
+#     print(UNITY_GET(model))
+
+# --------------------------------------------- #
+# -------------------- API -------------------- #
+# --------------------------------------------- #
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+def UNITY_GET(model):
+
+    varsthingy = {}
+    for agent in model.schedule.agent_buffer(False):
+        if agent.pos != None:
+            lane = agent.pos[0]
+        else:
+            lane = -1
+        varsthingy[agent.unique_id] = {
+            "id" : agent.unique_id,
+            "speed" : agent.speed,
+            "lane" : int(lane)
+        }
+
+    jsonOut = json.dumps(varsthingy, sort_keys=True)
+
+    model.step()
+
+    return jsonOut
+
+class Server(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        jsonOut = UNITY_GET(model)
+        
+        self._set_response()
+        
+        self.wfile.write(str(jsonOut).encode('utf-8'))
+        # self._set_response()
+        # self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+    
+    # def do_POST(self):
+    #     content_length = int(self.headers['Content-Length'])
+    #     #post_data = self.rfile.read(content_length)
+    #     post_data = json.loads(self.rfile.read(content_length))
+    #     #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+    #                 #str(self.path), str(self.headers), post_data.decode('utf-8'))
+    #     logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+    #                 str(self.path), str(self.headers), json.dumps(post_data))
+
+    #     jsonOut = UNITY_GET(model)
+        
+    #     self._set_response()
+
+    #     self.wfile.write(str(jsonOut).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=Server, port=8585):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info("Starting httpd...\n") # HTTPD is HTTP Daemon!
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:   # CTRL+C stops the server
+        pass
+    httpd.server_close()
+    logging.info("Stopping httpd...\n")
+
+if __name__ == '__main__':
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
 
 # --------------------------------------------- #
 # ----------------- ANIMATION ----------------- #
 # --------------------------------------------- #
 
-allGrid = model.datacollector.get_model_vars_dataframe()
-fig, axs = plt.subplots(figsize = (18, 4))
-axs.set_xticks([])
-axs.set_yticks([])
-patch = plt.imshow(allGrid.iloc[0][0], cmap=plt.cm.binary)
+# allGrid = model.datacollector.get_model_vars_dataframe()
+# fig, axs = plt.subplots(figsize = (18, 4))
+# axs.set_xticks([])
+# axs.set_yticks([])
+# patch = plt.imshow(allGrid.iloc[0][0], cmap=plt.cm.binary)
 
-def animate(i):
-    patch.set_data(allGrid.iloc[i][0])
+# def animate(i):
+#     patch.set_data(allGrid.iloc[i][0])
 
-anim = animation.FuncAnimation(fig, animate, frames=totalSteps)
-plt.show()
+# anim = animation.FuncAnimation(fig, animate, frames=totalSteps)
+# plt.show()
---
+++ server.py
@@ -0,0 +1,74 @@
+
+
+import json
+
+# --------------------------------------------- #
+# -------------------- API -------------------- #
+# --------------------------------------------- #
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+def UNITY_GET(model):
+
+    varsthingy = {}
+    for agent in model.schedule.agent_buffer(False):
+        if agent.pos != None:
+            lane = agent.pos[0]
+        else:
+            lane = -1
+        varsthingy[agent.unique_id] = {
+            "id" : agent.unique_id,
+            "speed" : agent.speed,
+            "lane" : int(lane)
+        }
+
+    jsonOut = json.dumps(varsthingy, sort_keys=True)
+
+    return jsonOut
+
+class Server(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        #post_data = self.rfile.read(content_length)
+        post_data = json.loads(self.rfile.read(content_length))
+        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                    #str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                    str(self.path), str(self.headers), json.dumps(post_data))
+
+        jsonOut = UNITY_GET()
+        
+        self._set_response()
+        #self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write(str(position).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=Server, port=8585):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info("Starting httpd...\n") # HTTPD is HTTP Daemon!
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:   # CTRL+C stops the server
+        pass
+    httpd.server_close()
+    logging.info("Stopping httpd...\n")
+
+if __name__ == '__main__':
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
